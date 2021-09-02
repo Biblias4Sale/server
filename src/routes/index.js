2d9687676c06400a71715ccaa030fe71d6f56e2b
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-02 16:34:22 -0300

    rutas corregidas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const networkCategory = require('../components/category/network')
-const networkLesson = require('../components/lesson/network')
+const networkProduct = require('../components/product/network')
 const login = require('../components/auth/login')
 const logout = require('../components/auth/logout')
 const user = require('../components/auth/user')
 
-
 router.use('/category', networkCategory)
-router.use('/lesson', networkLesson)
+router.use('/product', networkProduct)
 
-
-
-//auth
-router.use('/api/v1/', login);
-router.use('/logout', logout);
-router.use('/user', user);
+// auth
+router.use('/api/v1/', login)
+router.use('/logout', logout)
+router.use('/user', user)
 
 module.exports = router
--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const networkCategory = require('../components/category/network')
-const networkProduct = require('../components/product/network')
-
-router.use('/category', networkCategory)
-router.use('/product', networkProduct)
-
-module.exports = router
