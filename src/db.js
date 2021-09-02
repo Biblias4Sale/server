15551a2a549fad21f866b7d06ee3926794cef33f
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-02 16:26:26 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -8,13 +8,13 @@ const { DB_PG_USER, DB_PG_PASSWORD, DB_PG_HOST, DB_PG_PORT, DB_PG_DATABASE, DATA
 const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = isProduction ? DATABASE_URL : `postgres://${DB_PG_USER}:${DB_PG_PASSWORD}@${DB_PG_HOST}:${DB_PG_PORT}/${DB_PG_DATABASE}`
 
-const sequelize = new Sequelize(connectionString, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  ssl: {
-    rejectUnauthorized: false
-  }
-})
+// const sequelize = new Sequelize(connectionString, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// })
 
 //
 const basename = path.basename(__filename)
