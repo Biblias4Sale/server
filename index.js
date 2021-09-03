936c2608e964f7cc5744930a279a60bc976eacca
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-03 11:19:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const server = require('./src/app.js')
 const { conn } = require('./src/db.js')
 const { force } = require('./config.js')
 const PORT = process.env.PORT || 3001
+const { DATABASE_URL } = process.env
 
 process.stdout.write('\u001b[2J\u001b[0;0H') // limpia pantalla de la consola
 
@@ -16,5 +17,7 @@ process.stdout.write('\u001b[2J\u001b[0;0H') // limpia pantalla de la consola
 // })
 
 server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
+
+console.log(DATABASE_URL)
