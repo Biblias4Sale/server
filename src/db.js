0c6741ddb5eb09e5448377112b6b8a7e62371ca4
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-03 10:57:40 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -8,6 +8,8 @@ const { DB_PG_USER, DB_PG_PASSWORD, DB_PG_HOST, DB_PG_PORT, DB_PG_DATABASE, DATA
 const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = isProduction ? DATABASE_URL : `postgres://${DB_PG_USER}:${DB_PG_PASSWORD}@${DB_PG_HOST}:${DB_PG_PORT}/${DB_PG_DATABASE}`
 
+console.log('>>>', DATABASE_URL)
+
 const sequelize = new Sequelize(connectionString, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
