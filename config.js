9cdf0648e56953c08cecdb2a75905fbc9adc5b15
andresmonsa <monsalbefotografia@gmail.com>
2021-09-05 02:11:18 -0300

    Loader de Productos

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
   alter: true,
   force: true,
   categoriesList: ['Accesorios', 'Camaras', 'Lentes', 'Luces', 'Cargadores y baterías'],
-  subCategoryCamaras: ['Reflex', 'Mirrorless', 'Compacta'],
+  subCategoryCamaras: ['Reflex', 'Mirrorless', 'Compacta', 'Semi-Reflex'],
   subCategoryLentes: ['Zoom', 'Fijo'],
   subCategoryAccesorios: ['Filtros', 'Protectores', 'Cuidado y Limpieza'],
   subCategoryLuces: ['Flashes', 'Iluminadores Led', 'Modificadores'],
