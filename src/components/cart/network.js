bd4133ce5586e484548efc619aaad486c767ebf8
ciucol <contact@diegonaranjo.co>
2021-09-16 02:49:36 -0500

    Crea carrito. Adiciona producto. Falta actualizar precio de producto al hacer GET al carrito

--- src/components/cart/network.js
+++ src/components/cart/network.js
@@ -10,4 +10,11 @@ router.get('/:id', (req, res) => {
     .catch(e => response.error(req, res, 404, e, 'Cart not found'))
 })
 
+router.post('/addProduct/:id', (req, res) => {
+  controller
+    .addProduct(req.body)
+    .then(message => response.success(req, res, 200, message))
+    .catch(e => response.error(req, res, 404, e, 'Product not found'))
+})
+
 module.exports = router
--- src/components/cart/store.js
+++ src/components/cart/store.js
@@ -1,19 +1,42 @@
-const { Cart, User } = require('../../db')
+const { Cart, Product, User, ProductSold } = require('../../db')
 
 const getCart = async (id) => {
   try {
-    const cart = await Cart.findOne({ where: { UserId: id, status: 'En proceso' } })
+    const cart = await Cart.findOne({ where: { UserId: id, status: 'En proceso' }, include: ProductSold })
     if (!cart) {
       const user = User.findByPk(id)
       const cart = await user.createCart({ status: 'En proceso' })
+      // Actualizar precio de productos
       return cart.id
     }
-    return cart.id
+    return cart
   } catch (error) {
     return error
   }
 }
 
+const addProduct = async (cartId, productId, infoProduct) => {
+  try {
+    const cart = await Cart.findByPk(cartId)
+    const product = await Product.findByPk(productId)
+    const productSold = await ProductSold.findOne({ where: { CartId: cartId, productId: productId } })
+    if (!productSold) {
+      const newProductSold = await cart.createProductSold({ amount: infoProduct.amount, price: product.price, productId: productId })
+      return newProductSold
+    }
+    if (infoProduct.amount) {
+      productSold.amount = infoProduct.amount
+      productSold.save()
+      productSold.price = product.price
+      return 'Update product'
+    }
+    return 'Not change product'
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 module.exports = {
-  getCart
+  getCart,
+  addProduct
 }
