b04aa5f08747022d1bd98b40f08b5d00927d67c8
GotoxyDev <contact@gotoxy.dev>
2021-09-17 00:47:13 -0300

    encoded/img-upload-front-/preview

--- src/app.js
+++ src/app.js
@@ -17,8 +17,9 @@ server.name = 'NoiLan'
 // server.use(cors())
 // server.use(cors({ credentials: true }))
 server.use(cors({ origin: frontEndHost(), credentials: true }))
-server.use(express.json())
 server.use(morgan('dev'))
+server.use(express.json({limit: '50mb'}));
+
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', frontEndHost()) // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true')
@@ -26,9 +27,10 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
   next()
 })
+
+server.use(express.urlencoded({limit: '50mb'}));
 server.use(fileUpload())
 server.use(express.static('files'))
-
 server.use(cookieParser())
 server.use(session({ secret: 'anythinglol' }))
 server.use(passport.initialize())
