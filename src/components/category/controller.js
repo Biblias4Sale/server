e23172e3b3520f0d4a374beed30818094f7c7ba8
GotoxyDev <contact@gotoxy.dev>
2021-09-16 21:43:16 -0300

    need2update

--- src/components/category/controller.js
+++ src/components/category/controller.js
@@ -35,6 +35,8 @@ module.exports = {
 // producto:{
 //   name:'asdasd',
 //   id:'asdsadsd',
+//   brand:{
+//    name:'asdsadsa'
 //   category:{
 //     name:['asdasdds'],
 //     subCategory:{
@@ -42,20 +44,39 @@ module.exports = {
 //     }
 //   }
 // }
-// producto.category.subcatory.name
-
+// producto.category.subcatory.name.asdas.das.das.sad.
 // proucto:{
 //   name:'asdsad',
 //   id:'1',
 //   category:[],
 //   subcategory:'asdasd'
+//   brand:'asdasds
+//   review:'asdsad'
+//   img:[i1, i2, i2]
 // }
 // producto.subcatory
-
-
 // mariano 3
-// parma: 6,
+// parma: 5,
 // diego: 4,
 // lina: 2,
 // nacho: 2,
 // andres: 1
+
+
+
+
+
+const obj = {
+      model: 'SX100',
+      brand: 'Canon ',
+      img: 'https://arsonyb2c.vteximg.com.br/arquivos/ids/292451-550-550/ILCE-7M3_Black-1.jpg?v=637123589061300000',
+      description: 'Una cámara linda',
+      price: 800,
+      points: 5,
+      subCategory: 'Semi-Reflex'
+}
+
+const pago = async () => {
+  await axios.post("http://localhost3002/api/v1/mercadopago", {obj}, {withCredentials:true})
+  console.log(response)
+}
