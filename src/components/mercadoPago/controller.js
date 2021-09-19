767876eca1f88eaa05f316b89ea14c2cdc6ce053
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-19 19:01:11 -0300

    nada

--- src/components/mercadoPago/controller.js
+++ src/components/mercadoPago/controller.js
@@ -4,9 +4,9 @@ const mercadopago = require('mercadopago')
 
 const { ACCESS_TOKEN } = process.env
 
-// mercadopago.configure({
-//   access_token: ACCESS_TOKEN
-// })
+mercadopago.configure({
+  access_token: ACCESS_TOKEN
+})
 
 const getPaymentLink = async (request) => {
   let paymentLink
